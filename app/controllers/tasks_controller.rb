ec7102a9a8d06367a4982b0d28f23a65ec5f0784
EC2 Default User <ec2-user@ip-172-31-11-233.us-east-2.compute.internal>
2018-03-30 01:26:39 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329065426) do
+ActiveRecord::Schema.define(version: 20180330003508) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.text     "status",     limit: 65535
   end
 
 end
